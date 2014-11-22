8ed96e53fd356de15d7111c3eb4b44f32cb24a34
sniffy1988 <sniffy1988@gmail.com>
2014-11-22 12:05:03 +0200

    first commit add very early logic

---
+++ all.js
@@ -0,0 +1,18 @@
+var app = angular.module('firstModule',[]);
+app.controller('filmListController', function(){
+	this.films = ['one','two','three'];
+	this.film = '';
+	this.reccomend ='';
+	this.save = function(nameFilm){
+		 var film_list = nameFilm;
+		this.films.push(film_list);
+		this.film = '';
+	};
+	this.reset = function(){
+		this.film = '';
+	};
+	this.randomize = function(){
+		var randomNumber = Math.floor(Math.random() * (this.films.length - 0 + 1)) + 0;
+		this.reccomend = this.films[randomNumber];
+	}
+});
